7adab1e0e622bfc47597ed6352f9a4fcbcc0e141
som4n <61178177+som4n@users.noreply.github.com>
2025-02-21 20:02:30 +0000

    Update main.py

--- main.py
+++ main.py
@@ -148,7 +148,5 @@ def main():
                     sys.stdout.write(f"{command}: command not found\n")
                     sys.stdout.flush()
             
-
-            
 if __name__ == "__main__":
     main()
